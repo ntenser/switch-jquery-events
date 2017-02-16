c6129c930d2d34c0abc7bea946415218b2110345
noah tenser <ntenser@hotmail.com>
2017-02-16 16:32:51 -0500

    finished

--- js/main.js
+++ js/main.js
@@ -1,2 +1,20 @@
+$(function () {
+  $(".on, .off").bind('click', function() {
+      $(this).toggleClass('on off');
+  });
+
+  $(".light, .dark").bind('click', function() {
+      $(this).toggleClass('light dark');
+
+  });
 
 
+$('button').on('click', function() {
+  if ($('button').attr('class') === 'on') {
+  $('.status').html("It's so bright in here!") }
+
+  else {
+    $('.status').html("Who turned off the lights!") }
+
+  });
+});
